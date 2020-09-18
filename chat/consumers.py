0c4328ebd854843c4b4c40f4ddae683e34a28cff
Vishvaraj Dhanawade <vishvarajdhnwd120@gmail.com>
2020-09-18 21:08:05 +0530

    added username with message

--- chat/consumers.py
+++ chat/consumers.py
@@ -14,22 +14,38 @@ class ChatRoomConsumer(AsyncWebsocketConsumer):
 
         await self.accept()
 
-        await self.channel_layer.group_send(
-            self.room_group_name,
-            {
-                'type': 'tester_message',
-                'tester': 'hello world',
-            }
-        )
-
-    async def tester_message(self, event):
-        tester = event['tester']
-        await self.send(text_data=json.dumps({
-            'tester': tester
-        }))
-
     async def disconnect(self, close_code):
+        # disconnecting web socket connection
         await self.channel_layer.group_discard(
             self.room_group_name,
             self.channel_name
         )
+    
+    async def receive(self, text_data):
+        """receiving data from web page
+        """
+        text_data_json = json.loads(text_data)
+
+        # extracting data from key message sent from web page
+        message = text_data_json['message']
+        username = text_data_json['username']
+
+        # sending received message in group window
+        await self.channel_layer.group_send(
+            self.room_group_name,
+            {
+                'type': 'chatroom_message',
+                'message': message,
+                'username': username,
+            }
+        )
+
+    async def chatroom_message(self, event):
+        """sending message in group chat window"""
+        message = event['message']
+        username = event['username']
+
+        await self.send(text_data=json.dumps({
+            'message': message,
+            'username': username,
+        }))
